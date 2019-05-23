00de0cd11743ed410cbb391944852ba6ac746bf1
allan <allancruz716@gmail.com>
2019-05-23 00:45:05 -0300

    Stylize navbar and carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -237,7 +237,7 @@ export class HomeComponent implements OnInit {
     }
 
     initCarouselBanner() {
-        $('#carousel-banner').owlCarousel({
+        $('.carousel-banner-parent').owlCarousel({
 
             autoPlay: 3000,
             items: 1,
